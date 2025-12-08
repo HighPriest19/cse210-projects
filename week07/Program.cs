f48d7930080357e3ed12d8403d1601a92dab72b4
highpriest19 <kryptonolagunju@gmail.com>
2025-12-08 12:59:52 +0100

    commit

---
+++ week07/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ExerciseTracking;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+  
+        // Create a list of activities
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("8 Dec 2025", 30, 3.0),
+            new Cycling("8 Dec 2025", 30, 9.7),
+            new Swimming("8 Dec 2025", 30, 20)
+        };
+
+        // Loop through and display summaries
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+    }
+}
